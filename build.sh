fcbe1d5adcccb3d36156d44fd830df28ed4a7425
Brian Cook <bcook@redhat.com>
2023-10-18 15:41:09 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
-rpm-ostree compose image  \
---initialize-mode=if-not-exists \
---format=registry \
-../workstation-ostree-config/fedora-base.yaml \
-quay.io/bcook/ostree:fedora-base-$(date +%S)
+#rpm-ostree compose image  \
+#--initialize-mode=if-not-exists \
+#--format=registry \
+#../workstation-ostree-config/fedora-base.yaml \
+#quay.io/bcook/ostree:fedora-base-$(date +%S)
 
+rpm-ostree compose image \
+--initialize-mode if-not-exists \
+--format registry --authfile ./regauth \
+../workstation-ostree-config/fedora-base.yaml \
+quay.io/bcook/ostree:fedora-base-$(date +%s)
 
