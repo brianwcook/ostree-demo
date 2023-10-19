c3449479a0a8cee935248b36f4edc32598633e68
Brian Cook <bcook@redhat.com>
2023-10-19 08:34:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-dnf install -y git ostree rpm-ostree skopeo selinux-policy-targeted
+dnf install -y git ostree rpm-ostree skopeo selinux-policy-targeted podman skopeo
 git clone https://pagure.io/workstation-ostree-config.git
 https://github.com/brianwcook/ostree-demo
 git clone https://github.com/brianwcook/ostree-demo
